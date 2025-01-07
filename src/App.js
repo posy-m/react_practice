95832c7975b7a10fe4fedd624fbaea7a74df4432
posy-m <minji.posy@gmail.com>
2025-01-07 17:10:44 +0900

    조건적 콘텐츠 렌더링

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import TabButton from './componenets/TabButton';
+import { useState } from 'react';
+import { EXAMPLES } from './data.js'
+
+// const reactDescriptions = ['우리 다같이', '안녕', '후후']
+
+// function genRandomInt(max) {
+//   return Math.floor(Math.random() * (max + 1))
+// }
+
+// function Header() {
+//   const description = reactDescriptions[genRandomInt(2)]
+//   return (
+//     <header>
+//       <h1>React</h1>
+//       <p>{description} 다같이 손벽을!</p>
+//     </header>
+//   )
+// }
 
 function App() {
+
+  const [change, setChange] = useState(null)
+
+  function clickHandler(selectedBtn) {
+    // console.log(selectedBtn);
+    setChange(selectedBtn)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header /> */}
+
+      <section >
+        <h2>Examples</h2>
+        <menu>
+          <TabButton onSelect={() => clickHandler('components')}>Componenets</TabButton>
+          <TabButton onSelect={() => clickHandler('jsx')}>JSX</TabButton>
+          <TabButton onSelect={() => clickHandler('props')}>Props</TabButton>
+          <TabButton onSelect={() => clickHandler('state')}>State</TabButton>
+        </menu>
+        {!change ? <p>내용을 하나 선택해 주세요</p> : (<div>
+          <h3>{EXAMPLES[change].title}</h3>
+          <p>{EXAMPLES[change].description}</p>
+          <pre>
+            <code>
+              {EXAMPLES[change].code}
+            </code>
+          </pre>
+        </div>)}
+
+      </section>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const EXAMPLES = {
+  components: {
+    title: '1번 제목',
+    description: '1번내용',
+    code: '1번 코드 내용'
+  },
+  jsx: {
+    title: '2번 제목',
+    description: '2번내용',
+    code: '2번 코드 내용'
+  },
+  props: {
+    title: '3번 제목',
+    description: '3번내용',
+    code: '3번 코드 내용'
+  },
+  state: {
+    title: '4번 제목',
+    description: '4번내용',
+    code: '4번 코드 내용'
+  }
+}
